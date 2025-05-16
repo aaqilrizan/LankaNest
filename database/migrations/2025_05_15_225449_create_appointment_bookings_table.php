32edd1d41043dd11e348fe6118cbd62c99111a16
CB009882 <cb011205@students.apiit.lk>
2025-05-16 05:49:09 +0530

    Update 2025_05_15_225449_create_appointment_bookings_table.php

--- database/migrations/2025_05_15_225449_create_appointment_bookings_table.php
+++ database/migrations/2025_05_15_225449_create_appointment_bookings_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('property_id')->constrained()->onDelete('cascade');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('time_slot_id')->constrained()->onDelete('cascade');
+            $table->unsignedBigInteger('time_slot_id');
             $table->enum('status', ['pending', 'confirmed', 'cancelled'])->default('pending');
             $table->timestamps();
 
